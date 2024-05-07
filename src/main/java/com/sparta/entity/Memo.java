01bc50401363ea2bfd096bd60bf2efa1d2ff993b
lgsok00 <lgsok00@naver.com>
2024-05-07 10:38:03 +0900

    영속성 컨텍스트와 트랜잭션 이해하기

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -19,4 +19,28 @@ public class Memo {
   // length: 컬럼 길이 지정
   @Column(name = "contents", nullable = false, length = 500)
   private String contents;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getContents() {
+    return contents;
+  }
+
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
 }
