7a977dbcdc48df49456154b66f72996155b889ba
lgsok00 <lgsok00@naver.com>
2024-05-16 20:23:33 +0900

    엔티티의 상태 - 비영속 & 영속

---
+++ src/test/java/EntityStateTest.java
@@ -0,0 +1,50 @@
+import com.sparta.entity.Memo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class EntityStateTest {
+
+  EntityManagerFactory emf;
+  EntityManager em;
+
+  @BeforeEach
+  void setUp() {
+    emf = Persistence.createEntityManagerFactory("memo");
+    em = emf.createEntityManager();
+  }
+
+  @Test
+  @DisplayName("비영속과 영속 상태")
+  void test1() {
+    EntityTransaction et = em.getTransaction();
+
+    et.begin();
+
+    try {
+
+      Memo memo = new Memo(); // 비영속 상태
+      memo.setId(1L);
+      memo.setUsername("Robbie");
+      memo.setContents("비영속과 영속 상태");
+
+      em.persist(memo);  // 영속 상태
+
+      et.commit();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      et.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
+  }
+
+
+}
